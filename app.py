92552ace1cfcc761ed8987d481d9378ee420298e
MatejTheLes <matej.leskovsek.ce@gmail.com>
2021-11-28 16:25:06 +0100

    database implementation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import pymongo
 
 app = Flask(__name__)
 
@@ -10,4 +11,19 @@ def hello_world():
 @app.route('/authenticate', methods = ['POST'])
 def login():
     # connect to mongodb and authenticate user, return token
-    return("200OK")
+    try:
+        client = pymongo.MongoClient("mongodb+srv://admin:1337h4x0r@ecostreet.hqlgz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        db = client.ecostreetdb
+        user = db.users.find_one({
+            "username": request.form["username"]
+        })
+    except:
+        return "Error 500: Unable to connect to database."
+    try:
+        if(user["password"] == request.form["password"]):
+            return user["AccessToken"]
+        else:
+            return "Error 401: Unauthorized. Login Incorrect."
+    except:
+        return "Error 404: User Not Found."
+
