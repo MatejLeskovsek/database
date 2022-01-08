99a49ae937080b7c0c020fdc3f40ae06c21c7748
MatejTheLes <matej.leskovsek.ce@gmail.com>
2022-01-08 20:30:59 +0100

    Database update

--- app.py
+++ app.py
@@ -61,14 +61,6 @@ docs.register(health)
 @app.route("/db")
 @marshal_with(NoneSchema, description='200 OK', code=200)
 def hello_world():
-    syslog = SysLogHandler(address=('logs3.papertrailapp.com', 17630))
-    syslog.addFilter(ContextFilter())
-    format = '%(asctime)s %(hostname)s Time: %(message)s'
-    formatter = logging.Formatter(format, datefmt='%b %d %H:%M:%S')
-    syslog.setFormatter(formatter)
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.info("This is my log.")
     return {"response": "Database microservice."}, 200
 docs.register(hello_world)
 
