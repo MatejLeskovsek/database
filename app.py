45cdda483ea4afe6c959443c9f0b6a78634aefc7
MatejTheLes <matej.leskovsek.ce@gmail.com>
2022-01-08 22:24:19 +0100

    Database update

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def update_ip():
     try:
         url = 'http://' + configuration_core_service + '/cfupdate'
         response = requests.post(url, data=data)
-        logger.info(response)
+        logger.info(response.text)
         return {"response": response.text}, 200
     except:
         return {"response": "Something went wrong."}, 500
